d5ff45366e0b35a4569620f5e18527a22d22145c
NikitaTurlyko <nikita@kaptio.com>
2020-05-15 10:15:47 +0300

    fix sort logic

--- src/api/tableSortLogic.js
+++ src/api/tableSortLogic.js
@@ -6,20 +6,35 @@ export default function sortSourceData(fieldToSort, source) {
 		sortedArray = source
 	} else {
 		sortedArray = source.sort((a, b) => {
-			if (a[field].name) {
-				if (a[field].name > b[field].name) {
+			if (a[field] === null && b[field] === null) {
+				return 0
+			}
+
+			if (a[field] !== null && b[field] === null) {
+				return -1
+			}
+
+			if (a[field] === null && b[field] !== null) {
+				return 1
+			}
+
+			if (a[field] !== null && b[field] !== null) {
+				if (a[field].name) {
+					if (a[field].name > b[field].name) {
+						return reverse ? -1 : 1
+					}
+					if (a[field].name < b[field].name) {
+						return reverse ? 1 : -1
+					}
+					return 0
+				}
+				if (a[field] > b[field]) {
 					return reverse ? -1 : 1
 				}
-				if (a[field].name < b[field].name) {
+				if (a[field] < b[field]) {
 					return reverse ? 1 : -1
 				}
 				return 0
-			}
-			if (a[field] > b[field]) {
-				return reverse ? -1 : 1
-			}
-			if (a[field] < b[field]) {
-				return reverse ? 1 : -1
 			}
 			return 0
 		})
