ffbf8df369904a47b2dc9d356bbc04b7ebaf3022
hhh1361 <nikita.turlyko@gmail.com>
2020-05-16 15:39:01 +0300

    Updates

---
+++ precache-manifest.3363fa4771835b393789796d9cd4da0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9e49f651b081c232e90ffef3f6526e",
+    "url": "/parking-app/index.html"
+  },
+  {
+    "revision": "f3ebb427be78e64bea51",
+    "url": "/parking-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c0b89371512ecf667ec",
+    "url": "/parking-app/static/css/main.a1f8c834.chunk.css"
+  },
+  {
+    "revision": "f3ebb427be78e64bea51",
+    "url": "/parking-app/static/js/2.811af32f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/parking-app/static/js/2.811af32f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0b89371512ecf667ec",
+    "url": "/parking-app/static/js/main.987f744a.chunk.js"
+  },
+  {
+    "revision": "dca3fdf301e160ce690f",
+    "url": "/parking-app/static/js/runtime-main.40bcc960.js"
+  }
+]);
